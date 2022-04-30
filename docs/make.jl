b4ae48af1746645e5e0ffbf6d80ac3fabb749a63
kobied <kedelman@gmail.com>
2022-04-30 11:28:08 +0300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,14 @@ if Base.HOME_PROJECT[] !== nothing
     Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[])
 end
 
+isinstalled(pkg::String) = any(x -> x.name == pkg && x.is_direct_dep, values(Pkg.dependencies()))
+
+using Pkg
+
+if !isinstalled("Documenter")
+    Pkg.add("Documenter")
+end
+
 using Documenter, PointLib
 
 makedocs(
